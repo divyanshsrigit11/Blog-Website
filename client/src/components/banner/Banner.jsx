eacaab998a43c31d4e7aff5ce05f24ab4b84f8dc
Divyansh Srivastava <divyanshsri1101@gmail.com>
2025-05-13 00:39:08 +0530

    Update Banner.jsx

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -27,7 +27,7 @@ const Banner = () => {
     return (
         <Image>
             <Heading>BLOG</Heading>
-            <SubHeading>Code for Interview</SubHeading>
+            <SubHeading>Divyansh Srivastava</SubHeading>
         </Image>
     )
 }
