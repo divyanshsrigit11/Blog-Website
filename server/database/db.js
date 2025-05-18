81e32d277f917951476134e4f79bc636a4d4f3a4
Divyansh Srivastava <divyanshsri1101@gmail.com>
2025-05-18 22:23:47 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose"
 
-const Connection = async (username, password) => {
-    const URL = `mongodb://${username}:${password}@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    try {
-        await mongoose.connect(URL, { useNewUrlParser: true })
-        console.log('Database connected successfully');
+
+
+export const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@blogapp.pmsdvia.mongodb.net/?retryWrites=true&w=majority&appName=BlogApp`;
+    try{
+        await mongoose.connect(URL, { useNewUrlParser: true });
+        console.log('Database connected Successfully');
     } catch (error) {
-        console.log('Error while connecting to the database ', error);
+        console.log('Error while connecting Database', error);
     }
-};
+}
 
 export default Connection;
